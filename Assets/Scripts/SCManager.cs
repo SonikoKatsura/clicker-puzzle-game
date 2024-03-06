baa4d8a458e28d15979555f227001a086503def8
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2024-03-06 17:20:20 +0000

    feat: Make SceneManager a Singleton

--- Assets/Scripts/SCManager.cs
+++ Assets/Scripts/SCManager.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SCManager : MonoBehaviour {
+    public static SCManager instance;
+    void Start() {
+        if (instance == null) instance = this;
+        else { Destroy(gameObject); return; }
+        DontDestroyOnLoad(gameObject);
+    }
 
     void LoadScene(string name) {
         SceneManager.LoadScene(name);
